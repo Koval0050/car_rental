4e5b1efc3d1750fcfd6c5fb8d10f20d1c5a3b130
Koval0050 <121100028+Koval0050@users.noreply.github.com>
2023-11-30 16:53:07 +0200

    try to fix github pages

--- src/App.js
+++ src/App.js
@@ -1,18 +1,7 @@
-import "./App.css";
-
-import { Container } from "./component/common/Container/Container.styled";
-import FilterBar from "./component/FilterBar/FilterBar";
-
-import CarsCardList from "./component/CarsCard/CarsCardList";
-
 function App() {
   return (
     <div className="App">
-      <Container>
-        <FilterBar />
-
-        <CarsCardList/>
-      </Container>
+      <h2>Hello</h2>
     </div>
   );
 }
